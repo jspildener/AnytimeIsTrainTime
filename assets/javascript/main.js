84258f9d1af437b2c0968b9ecb8706e8cd013f34
William R Poulson <jessica.spildener@gmail.com>
2017-08-28 20:08:53 -0400

    input validation added

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -10,14 +10,39 @@ var config = {
 firebase.initializeApp(config);
 var database = firebase.database();
 
-//button to add the trains to table
+function validateInput(newTrain) {
+	$("#nameError").hide();
+	$("#destinationError").hide();
+	$("#timeError").hide();
+	$("#frequencyError").hide();
+   
+   var isValid = true;
+    if (newTrain.name == "") {
+        $("#nameError").show();
+        isValid = false;
+    }
+        if (newTrain.destination == "") {
+        $("#destinationError").show();
+        isValid = false;
+    }
+        if (newTrain.time == "") {
+        $("#timeError").show();
+        isValid = false;
+    }
+        if (newTrain.frequency == "") {
+        $("#frequencyError").show();
+        isValid = false;
+    }
+ 
+    return isValid;
+}
 
 $("#submitTrain").on("click", function(event) {
     event.preventDefault();
 
     var trainName = $("#trainName").val().trim();
     var destination = $("#destination").val().trim();
-    var trainTime = $("#trainTime").val().trim(); //need to make this a moment to change to am/pm
+    var trainTime = $("#trainTime").val().trim();
     var trainFrequency = $("#trainFrequency").val().trim();
 
     var newTrain = {
@@ -26,22 +51,29 @@ $("#submitTrain").on("click", function(event) {
         time: trainTime,
         frequency: trainFrequency
     };
-    database.ref().push(newTrain);
 
-    //make sure that submit train button works properly
-    console.log(newTrain.name);
-    console.log(newTrain.destination);
-    console.log(newTrain.time);
-    console.log(newTrain.frequency);
+    var submissionIsValid = validateInput(newTrain);
 
-    $("#trainName").val("");
-    $("#destination").val("");
-    $("#trainTime").val("");
-    $("#trainFrequency").val("");
+    if (submissionIsValid) {
+        database.ref().push(newTrain);
+
+        //make sure that submit train button works properly
+        console.log(newTrain.name);
+        console.log(newTrain.destination);
+        console.log(newTrain.time);
+        console.log(newTrain.frequency);
+
+        $("#trainName").val("");
+        $("#destination").val("");
+        $("#trainTime").val("");
+        $("#trainFrequency").val("");
+    } else{
+    	clearErrors();
+    }
 });
 
 function getMinutesAway(newTime, newFrequency) {
-   
+
     var diffTime = moment().diff(moment(newTime), "minutes");
     var trainRemainder = diffTime % newFrequency;
     var minutesAway = newFrequency - trainRemainder;
